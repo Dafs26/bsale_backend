d3e84535bcdd9e4a8604ec9d6649a03079f70b82
David Fernández <Dafs@localhost.localdomain>
2021-10-18 03:05:52 -0300

    adding cors whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -139,3 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = 'media/'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+]
